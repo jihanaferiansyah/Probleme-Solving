e842f6611ac098d3ab116d9a1abdae26d7ef5de7
jihanaferiansyah <jihanaferian@gmail.com>
2021-10-10 22:28:04 +0700

    problem 3

--- Assets/Scripit/Movement.cs
+++ Assets/Scripit/Movement.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    private Rigidbody2D Rb;
+    private Rigidbody2D Rb2D;
     public float speed = 500;
 
     void Start()
     {
-        Rb = GetComponent<Rigidbody2D>();
-        Rb.AddForce(new Vector2(20 * Time.deltaTime * speed, 20 * Time.deltaTime * speed));
+        Rb2D = GetComponent<Rigidbody2D>();
+        Rb2D.AddForce
+            (new Vector2(20 * Time.deltaTime * speed, 20 * Time.deltaTime * speed));
     }
 }
