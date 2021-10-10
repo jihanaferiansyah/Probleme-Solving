863aba87b80415e43917a4eb4d5c01ec4a9bc1a6
jihanaferiansyah <jihanaferian@gmail.com>
2021-10-10 21:47:00 +0700

    problem2

---
+++ Assets/Scrpit/Movement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody2D Rb;
+    public float speed = 500;
+
+    void Start()
+    {
+        Rb = GetComponent<Rigidbody2D>();
+        Rb.AddForce(new Vector2(20 * Time.deltaTime * speed, 20 * Time.deltaTime * speed));
+    }
+}
